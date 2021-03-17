d9b6d8c98e2b126c3bda41a33f3f38b91b0d420f
Tim Fitzgerald <diceytroop@gmail.com>
2021-03-17 05:34:09 -0400

    Improved best practices compliance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export { renderEvents } from './getText';
+import { renderEvents } from './getText';
 export * from './eventTypes';
 export * from './types';
+
+export default {
+  renderEvents,
+};
